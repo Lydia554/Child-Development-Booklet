7f8688cec24c29b2cfa77407be406dc99ca59156
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-03-12 13:33:23 +0100

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const cors = require('cors'); // Uvoz CORS-a
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes'); 
 const milestoneRoutes = require('./routes/milestoneRoutes');
@@ -10,7 +10,26 @@ const developmentRoutes = require('./routes/developmentRoutes');
 
 const app = express();
 const port = process.env.PORT || 5000; 
-app.use(cors());
+
+// CORS konfiguracija
+const allowedOrigins = [
+  'https://child-development-frontend.fly.dev', // Dozvolite vaš frontend domen
+  'http://localhost:8080', // Dozvolite vaš lokalni razvojni domen ako je potrebno
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Nije dozvoljeno od strane CORS-a'));
+    }
+  },
+  credentials: true, // Dozvolite kolačiće ili autorizacione zaglavlja
+};
+
+// Primena CORS middleware sa specifičnim opcijama
+app.use(cors(corsOptions)); 
 app.use(bodyParser.json()); 
 app.use(express.json()); 
 
@@ -28,7 +47,7 @@ if (!process.env.MONGO_URI) {
   process.exit(1);
 }
 
-console.log("🔄 Povezivanje sa MongoDB bazom...");
+console.log("🔄 Povezivanje sa MongoDB...");
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,

ff618f45fb934deee6cc7fa41b35772bc866d691
Lydia554 <73587947+Lydia554@users.noreply.github.com>
2025-03-12 15:45:24 +0100

    Update

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,11 +1,15 @@
-
 import axios from 'axios';
 
 axios.interceptors.request.use(config => {
   const token = sessionStorage.getItem('token');
+  
   if (token) {
+    console.log("🔑 Token found and added to request:", token); // Log the token to check
     config.headers['Authorization'] = `Bearer ${token}`;
+  } else {
+    console.warn("⚠️ No token found in sessionStorage.");
   }
+
   return config;
 }, error => {
   return Promise.reject(error);
